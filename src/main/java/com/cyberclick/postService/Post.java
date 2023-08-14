e1b06f50978db546b465c2f254d676b1bcc6e6fb
Sammy Ndabo <ndabosam084@gmail.com>
2023-08-14 23:37:28 +0200

    Model & Controller

---
+++ src/main/java/com/cyberclick/postService/Post.java
@@ -0,0 +1,28 @@
+package com.cyberclick.postService;
+
+public class Post {
+
+    private String postId;
+    private String description;
+    public Post(String postId, String description) {
+        this.postId = postId;
+        this.description = description;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
---
+++ src/main/java/com/cyberclick/postService/PostController.java
@@ -0,0 +1,16 @@
+package com.cyberclick.postService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/post")
+public class PostController {
+    @GetMapping(value = "/{postId}")
+    public Post getPost(@PathVariable("postId") String postId){
+        Post postOne = new Post(postId, "Description for "+postId);
+        return postOne;
+    }
+}
